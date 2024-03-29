09506940d113ff2fe0f0b2642a299d06e22bfd62
devu11 <devudevzz3@gmail.com>
2024-03-29 13:11:29 +0530

    url changed

--- src/api/AdminRequest.js
+++ src/api/AdminRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:3001"});
+const API = axios.create({baseURL: "https://circlify.shop/"});
 
 export const adminLogIn = (formData)=> API.post('/adminLogin',formData);
 
--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://circlify.shop/" });
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
 
