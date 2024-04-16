290384b818ecdf26fc918b6d9eb4fbbfe6e98992
devu11 <devudevzz3@gmail.com>
2024-04-16 13:09:54 +0530

    api added

--- src/api/UploadRequest.js
+++ src/api/UploadRequest.js
@@ -4,7 +4,7 @@ const API = axios.create({baseURL: "https://circlify.shop"})
 
 export const uploadImage = (postData) => API.post('/upload/',postData)
 
-export const uploadVideo = (data) => API.post('/upload', data);
+export const uploadVideo = (data) => API.post('/upload/', data);
 
 
 export const uploadPost = (data)=> API.post('/post',data)
